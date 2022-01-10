26dc6fc006f497180c9ae4fa505a0e625319c8e5
Dung Tran <pdung1989@gmail.com>
2022-01-10 23:05:11 +0200

    break into small components

---
+++ components/ListItem.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {View, Text, TouchableOpacity, Image, StyleSheet, TextPropTypes} from 'react-native';
+import PropTypes from 'prop-types';
+
+
+const ListItem = (props) => {
+  return (
+    <TouchableOpacity style={styles.row}>
+      <View style={styles.imageBox}>
+        <Image style={styles.image} source={{uri: props.singleItem.thumbnails.w160}} />
+      </View>
+
+      <View style={styles.textBox}>
+        <Text style={styles.title}>{props.singleItem.title}</Text>
+        <Text>{props.singleItem.description}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    backgroundColor: '#cea0e8',
+    padding: 20,
+    alignItems: 'flex-start',
+    borderRadius: 6,
+    marginHorizontal: 10,
+    marginBottom: 5,
+  },
+  textBox: {
+    fontSize: 20,
+    flex: 1,
+    paddingLeft: 15
+  },
+  imageBox: {
+    flex: 1,
+  },
+  image:{
+    flex: 2,
+    borderRadius: 6,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight:  'bold',
+  }
+});
+
+ListItem.propTypes = {
+  singleItem: PropTypes.object.isRequired,
+}
+
+export default ListItem;
