9a926b5944691abdf569e0d1baa1f52792cbbe74
Dung Tran <pdung1989@gmail.com>
2022-01-15 21:35:58 +0200

    style footer

--- App.js
+++ App.js
@@ -9,51 +9,76 @@ import {
 } from 'react-native';
 import GlobalStyles from './utils/GlobalStyles';
 import List from './components/List';
+import {Menu, Settings, Home, PlusSquare, Feather} from 'react-native-feather';
 
 const App = () => {
   return (
     <SafeAreaView style={GlobalStyles.AndroidSafeArea}>
-      <StatusBar backgroundColor="orange" barStyle="light-content" />
+      <StatusBar backgroundColor="#fafad2" barStyle="light-content" />
       <View style={styles.header}>
         <ImageBackground
           source={require('./assets/kittens.jpeg')}
           style={styles.bgImage}
           imageStyle={{resizeMode: 'cover', borderBottomRightRadius: 65}}
         ></ImageBackground>
+        <Menu width={40} height={40} stroke="#ffd700" style={styles.menu} />
+        <Settings
+          width={40}
+          height={40}
+          stroke="#ffd700"
+          style={styles.setting}
+        />
         <Text style={styles.hello}>Hello Kittens!</Text>
       </View>
       <List style={styles.infoArea} />
-      <View style={styles.footerArea} />
+      <View style={styles.footerArea}>
+        <Home width={35} height={35} stroke="#ffd700" />
+        <PlusSquare width={35} height={35} stroke="#ffd700" />
+        <Feather width={35} height={35} stroke="#ffd700" />
+      </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   header: {
-    height: 270,
-    backgroundColor: 'white',
+    height: 230,
+    backgroundColor: '#fafad2',
+  },
+  menu: {
+    position: 'absolute',
+    left: 30,
+    top: 30,
+  },
+  setting: {
+    position: 'absolute',
+    right: 30,
+    top: 30,
   },
   bgImage: {
     width: '100%',
-    height: 270,
+    height: 230,
   },
   hello: {
     position: 'absolute',
-    top: 200,
+    top: 150,
     fontSize: 20,
     fontWeight: 'bold',
-    backgroundColor: '#f0e68c',
+    backgroundColor: '#ffd700',
     padding: 10,
-    color: '#008000',
+    color: 'black',
   },
   infoArea: {
     flex: 6,
     flexDirection: 'row',
-    backgroundColor: 'skyblue',
+    marginTop: 50,
   },
   footerArea: {
-    flex: 1,
-    backgroundColor: 'steelblue',
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#32cd32',
   },
 });
 export default App;
--- components/ListItem.js
+++ components/ListItem.js
@@ -1,19 +1,29 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
 import PropTypes from 'prop-types';
-
 
 const ListItem = (props) => {
   return (
     <TouchableOpacity style={styles.row}>
       <View style={styles.imageBox}>
-        <Image style={styles.image} source={{uri: props.singleItem.thumbnails.w160}} />
+        <Image
+          style={styles.image}
+          source={{uri: props.singleItem.thumbnails.w160}}
+          imageStyle={{resizeMode: 'cover'}}
+        />
       </View>
 
-      <View style={styles.textBox}>
+      <ScrollView style={styles.textBox}>
         <Text style={styles.title}>{props.singleItem.title}</Text>
         <Text>{props.singleItem.description}</Text>
-      </View>
+      </ScrollView>
     </TouchableOpacity>
   );
 };
@@ -21,33 +31,33 @@ const ListItem = (props) => {
 const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
-    backgroundColor: '#cea0e8',
+    backgroundColor: '#ffd700',
     padding: 20,
     alignItems: 'flex-start',
     borderRadius: 6,
-    marginHorizontal: 10,
     marginBottom: 5,
+    height: 150,
   },
   textBox: {
     fontSize: 20,
     flex: 1,
-    paddingLeft: 15
+    paddingLeft: 15,
   },
   imageBox: {
     flex: 1,
   },
-  image:{
-    flex: 2,
-    borderRadius: 6,
+  image: {
+    flex: 3,
+    borderBottomLeftRadius: 50,
   },
   title: {
     fontSize: 20,
-    fontWeight:  'bold',
-  }
+    fontWeight: 'bold',
+  },
 });
 
 ListItem.propTypes = {
   singleItem: PropTypes.object.isRequired,
-}
+};
 
 export default ListItem;
