e882f2ccdfda51bac8588be4dfca71a6dc321dc7
Dung Tran <pdung1989@gmail.com>
2022-01-22 15:02:27 +0200

    pass object to the value of context provider

--- views/Profile.js
+++ views/Profile.js
@@ -1,19 +1,18 @@
 import React, {useContext} from 'react';
-import {StyleSheet, SafeAreaView, Text} from 'react-native';
+import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
 import {MainContext} from '../contexts/MainContext';
 import PropTypes from 'prop-types';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = ({navigation}) => {
   const {setIsLoggedIn} = useContext(MainContext);
-  console.log('profile isLoggedIn', isLoggedIn);
 
   const logout = async () => {
     setIsLoggedIn(false);
     await AsyncStorage.clear();
     navigation.navigate('Login');
   };
-  
+
   return (
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
