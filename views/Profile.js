d005255fc84f65771b494cfca7079f3e95c7fde8
Dung Tran <pdung1989@gmail.com>
2022-01-26 09:39:40 +0200

    set avatar useing state

--- views/Profile.js
+++ views/Profile.js
@@ -5,7 +5,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Profile = () => {
   const {setIsLoggedIn, user} = useContext(MainContext);
-  
+  const [avatar, setAvatar] = useState('http://placekitten.com/640');
+
   const logout = async () => {
     await AsyncStorage.clear();
     setIsLoggedIn(false);
@@ -15,6 +16,11 @@ const Profile = () => {
     <SafeAreaView style={styles.container}>
       <Text>Profile</Text>
       <Text>{user.username}</Text>
+      <Image
+        source={{uri: avatar}}
+        style={{width: '90%', height: '80%'}}
+        resizeMode="contain"
+      />
       <Text>{user.email}</Text>
       <Text>{user.full_name}</Text>
       <Button title={'Logout!'} onPress={logout} />
