fd7ce35c655db3f16d99a77fb3056413f7884a87
Dung Tran <pdung1989@gmail.com>
2022-01-27 09:04:37 +0200

    remove import Input

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -4,7 +4,6 @@ import {useForm, Controller} from 'react-hook-form';
 import {MainContext} from '../contexts/MainContext';
 import {useLogin} from '../hooks/ApiHooks';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import {Input} from 'react-native-elements';
 
 const LoginForm = () => {
   const {setIsLoggedIn, setUser} = useContext(MainContext);
