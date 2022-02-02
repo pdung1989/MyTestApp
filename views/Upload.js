d3e506a0b5fb5da3960eb283e026020a38292ac0
Dung Tran <pdung1989@gmail.com>
2022-02-02 15:15:17 +0200

    use postMedia to upload media

--- views/Upload.js
+++ views/Upload.js
@@ -1,14 +1,20 @@
 import React, {useState} from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
+import {Alert, ScrollView, StyleSheet} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 import PropTypes from 'prop-types';
 import {Button, Input, Text, Card} from 'react-native-elements';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import * as ImagePicker from 'expo-image-picker';
+import {useMedia} from '../hooks/ApiHooks';
 
 const Upload = ({navigation}) => {
   const [image, setImage] = useState(
     'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.iconsdb.com%2Fcaribbean-blue-icons%2Fupload-2-icon.html&psig=AOvVaw3E7CSaR2mf3WIytf6P2dkE&ust=1643889762624000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJiIgN784PUCFQAAAAAdAAAAABBV'
   );
+  const [type, setType] = useState('');
+  const [imageSelected, setImageSelected] = useState(false);
+  const {postMedia} = useMedia;
+
   const {
     control,
     handleSubmit,
@@ -20,6 +26,7 @@ const Upload = ({navigation}) => {
     },
   });
 
+  //choose image from the phone
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -33,6 +40,34 @@ const Upload = ({navigation}) => {
 
     if (!result.cancelled) {
       setImage(result.uri);
+      setImageSelected(true);
+    }
+  };
+
+  const onSubmit = async (date) => {
+    if (!imageSelected) {
+      Alert.alert('Please select a file');
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append('title', data.title);
+    formData.append('description', data.description);
+    const filename = image.split('/').pop();
+    let fileExtention = filename.split('.').pop();
+    fileExtention = fileExtention === 'jpg' ? 'jpeg' : 'jpeg';
+    formData.append('file', {
+      uri: image,
+      name: filename,
+      type: type + '/' + fileExtention,
+    });
+    // console.log(formData);
+    try {
+      const token = await AsyncStorage.getItem('userToken');
+      const response = await postMedia(formData, token);
+      console.log('upload response', response)
+    } catch (error) {
+      console.log('Upload media problem')
     }
   };
 
@@ -81,7 +116,7 @@ const Upload = ({navigation}) => {
         {errors.description && <Text>This is required.</Text>}
 
         <Button title="Choose image" onPress={pickImage} />
-        <Button title="Upload image" onPress={() => {}} />
+        <Button title="Upload image" onPress={handleSubmit(onSubmit)} />
       </Card>
     </ScrollView>
   );
